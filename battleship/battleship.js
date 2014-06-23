dba99f928190b719846a7f9d9b0bf09f48d64111
Jose Contreras <jocontre@joselcontreras.com>
2014-06-22 22:48:36 -0700

    Battleship JS

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -8,8 +8,25 @@ var guesses = 0;
 
 var isSunk = false;
 
-while(isSunk == false){
-	guess = prompt("What do you guess?");
+while (isSunk == false){
+	
+	guess = prompt("Enter a number (0-6)");
 
+	if (guess < 0 || guess > 6){
+		alert("Enter a valid number.");
+	}
 
+	else {
+		guess += 1
+
+		if ( guess == location1 || guess == location2 || guess == location3){
+		hits += 1;
+
+			if (hits == 3){
+				isSunk = true
+				alert("You sank my battleship!");
+			}
+		}
+	}
+	return
 }
