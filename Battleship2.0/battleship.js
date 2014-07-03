b9018c1389718d4496252e846bff9fde1dd5a305
Jose Contreras <jocontre@joselcontreras.com>
2014-07-02 22:36:36 -0700

    JS file

---
+++ Battleship2.0/battleship.js
@@ -0,0 +1,10 @@
+var view = {
+	displaymessage: function(msg) {
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+	displayHit: function(location){
+	},
+	displayMiss: function(location){
+	}
+};
