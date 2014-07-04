c3fca1cd0b4ccda4fe4f486b63850698561c34d2
Jose Contreras <jocontre@joselcontreras.com>
2014-07-03 23:51:06 -0700

    Onload callback

--- Battleship2.0/battleship.js
+++ Battleship2.0/battleship.js
@@ -175,3 +175,20 @@ function handleKeyPress(e) {
 		return false;
 	}
 };
+
+// init - called when the page has completed loading
+
+window.onload = init;
+
+function init() {
+	// Fire! button onclick handler
+	var fireButton = document.getElementById("fireButton");
+	fireButton.onclick = handleFireButton;
+
+	// handle "return" key press
+	var guessInput = document.getElementById("guessInput");
+	guessInput.onkeypress = handleKeyPress;
+
+	// place the ships on the game board
+	model.generateShipLocations();
+};
