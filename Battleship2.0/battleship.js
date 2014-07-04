0762864ee458ee8857a5c7d928e28ebdf1a5b5cc
Jose Contreras <jocontre@joselcontreras.com>
2014-07-03 23:11:57 -0700

    Finish

--- Battleship2.0/battleship.js
+++ Battleship2.0/battleship.js
@@ -11,8 +11,32 @@ var model = {
 	],
 
 	fire : function(guess){
-		
-	}
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			var index = ship.locations.indexOf(guess);
+
+			// here's an improvement! Check to see if the ship
+			// has already been hit, message the user, and return true.
+			if (ship.hits[index] === "hit") {
+				view.displayMessage("Oops, you already hit that location!");
+				return true;
+			} else if (index >= 0) {
+				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("HIT!");
+
+				if (this.isSunk(ship)) {
+					view.displayMessage("You sank my battleship!");
+					this.shipsSunk++;
+				}
+				return true;
+			}
+		}
+	},
+
+	view.displayMiss(guess);
+	view.displaymessage("You miss");
+	return false;
 }
 
 var view = {
