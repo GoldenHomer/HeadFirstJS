a8485efd2aaf24a9b3a359b80340018331d487cb
Jose Contreras <jocontre@joselcontreras.com>
2014-07-03 23:07:23 -0700

    Adding model

--- Battleship2.0/battleship.js
+++ Battleship2.0/battleship.js
@@ -1,3 +1,20 @@
+var model = {
+	boardsize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+
+	ships: [
+	{locations: [0,0,0], hits: ["","",""]},
+	{locations: [0, 0, 0], hits: ["", "", ""]},
+	{locations: [0, 0, 0], hits: ["", "", ""]}
+	],
+
+	fire : function(guess){
+		
+	}
+}
+
 var view = {
 	displaymessage: function(msg) {
 		var messageArea = document.getElementById("messageArea");
