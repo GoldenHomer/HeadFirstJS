2a6f29bde1db6c82c6bd208d9d45af0f52e82092
Jose Contreras <jocontre@joselcontreras.com>
2014-07-03 23:47:11 -0700

    Controller added

--- Battleship2.0/battleship.js
+++ Battleship2.0/battleship.js
@@ -117,3 +117,18 @@ var view = {
 		cell.setAttribute("class","miss");
 	}
 };
+
+var controller = {
+	guesses: 0,
+
+	processGuess: function(guess) {
+		var location = parseGuess(guess);
+		if (location) {
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips) {
+					view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+			}
+		}
+	}
+};
