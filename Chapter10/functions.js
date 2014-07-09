07194eb898513bb5aabb2324e0c5675487d66784
Jose Contreras <jocontre@joselcontreras.com>
2014-07-09 14:55:14 -0700

    New chapter file

---
+++ Chapter10/functions.js
@@ -0,0 +1,18 @@
+var migrating = true;
+
+var fly = function(num) {
+	for (var i = 0; i < num.length; i++) {
+		console.log("Flying!");
+	}
+};
+
+function quack(num){
+	for (var i = 0; i < num.length; i++) {
+		console.log("Quack!");
+	};
+}
+
+if (migrating) {
+	quack(4);
+	fly(4);
+};
