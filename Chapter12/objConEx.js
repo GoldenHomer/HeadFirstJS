a7808afea37d5b180e1a31a6eaf49caace413806
Jose Contreras <jocontre@joselcontreras.com>
2014-07-24 16:21:15 -0700

    Constructed some new objects

--- Chapter12/objConEx.js
+++ Chapter12/objConEx.js
@@ -12,3 +12,13 @@ var widgetA = new widget(100,"large");
 var widgetB = new widget(101, "small");
 var formFactorA = new FormFactor("plastic", widgetA);
 var formFactorB = new FormFactor("metal", widgetB);
+
+var fido = new Dog("Fido", "Mixed", 38);
+var fluffy = new Dog("Fluffy", "Poddle", 30);
+var spot = new Dog("Spot". "Chihuahua", 10);
+
+var dogs = [fido, fluffy, spot];
+
+for(var i = 0l i < dogs.length; i++){
+	dogs[i].bark();
+}
